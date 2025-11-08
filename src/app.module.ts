7eb3868cae532fe3d310c4a3e4c1d64449eeef50
Andrei Dyminski <smartfox87@gmail.com>
2025-11-08 11:20:14 +0100

    generate users and offers modules with crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, OffersModule],
   controllers: [AppController],
   providers: [AppService],
 })
