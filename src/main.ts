a0801dc8a7f5859ee4b9a17119bbd3688b237ad7
Andrei Dyminski <smartfox87@gmail.com>
2025-11-10 20:01:47 +0100

    add global serialization and fix validation config

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ValidationPipe, ClassSerializerInterceptor } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { setupSwagger } from './utils/swagger.util';
 
@@ -9,9 +9,12 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
+      forbidNonWhitelisted: true,
       transform: true,
     }),
   );
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
 
   setupSwagger(app);
 
